6d40e63f70e82fe171df099caa358b4e2e18c2b7
Palma <angelonovpalma29@gmail.com>
2024-10-26 18:09:49 -0600

    Se re-estructuro el programa

--- src/main/java/com/angelopalma/financialManagementAPI/config/repository/category/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.angelopalma.financialManagementAPI.config.repository.category;
-
-import com.angelopalma.financialManagementAPI.config.model.Category;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends MongoRepository<Category, String> {
-}
---
+++ src/test/java/com/angelopalma/library/BibliotecaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.angelopalma.library;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
